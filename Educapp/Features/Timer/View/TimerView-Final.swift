bd89013046f9291547a927d44d49c4bd9a7eb169
João Demari <joaodemari1@gmail.com>
2023-11-20 10:48:04 -0300

    demari first commit

---
+++ Educapp/Features/Timer/View/TimerView-Final.swift
@@ -0,0 +1,17 @@
+//
+//  TimerView-Final.swift
+//  Educapp
+//
+//  Created by Foundation13 on 20/11/23.
+//
+
+import SwiftUI
+
+
+struct TimerView : View{
+    var body: some View{
+        HStack{
+            Text("hiiii")
+        }
+    }
+}
