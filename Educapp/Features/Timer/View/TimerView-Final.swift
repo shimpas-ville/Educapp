94507f3ff6c376cf6c510ae4f29bae6e154438e6
João Demari <joaodemari1@gmail.com>
2023-11-20 11:23:42 -0300

    init of the timer fucntion

--- Educapp/Features/Timer/View/TimerView-Final.swift
+++ Educapp/Features/Timer/View/TimerView-Final.swift
@@ -8,10 +8,39 @@
 import SwiftUI
 
 
-struct TimerView : View{
-    var body: some View{
-        HStack{
-            Text("hiiii")
+struct TimerView: View {
+    @State var timeRemainingNumber = 1500
+    @State var timeRemaining = ""
+    let timer = Timer
+                .publish(every: 1, on: .main, in: .common).autoconnect()
+
+    var body: some View {
+        ZStack{
+            Circle()
+                .fill(.green)
+                .frame(width: 350, height: 350)
+            Circle()
+                .fill(.black)
+                .frame(width: 300, height: 300)
+            Text("\(timeRemaining)")
+                .onReceive(timer) { _ in
+                    minus1second()
+                }
+                .foregroundStyle(.white)
+                .fontWeight(.bold)
+                .font(.system(size: 72))
         }
     }
+    
+    func minus1second(){
+        if timeRemainingNumber > 0 {
+            timeRemainingNumber -= 1
+            timeRemaining = "\(timeRemainingNumber/60):\(timeRemainingNumber % 60)"
+        }
+    }
+}
+
+
+#Preview{
+    TimerView()
 }
